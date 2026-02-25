e4d11d65db27c187c8f7fac34b020ecb4f1cf9f2
EmanueleIppolito <emanuele2410@gmail.com>
2026-02-25 18:52:27 +0100

    Bonus completato

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function debounce(callback, delay){
 function App() {
   const [query, setQuery] = useState("")
   const [suggestions, setSuggestions] = useState([])
+  const [selectedId, setSelectedId] = useState(null)
+  const [selectedProduct, setSelectedProduct] = useState(null)
 
   const fetchProducts = useCallback(async (value) => {
     try{
@@ -41,11 +43,36 @@ function App() {
   [fetchProducts]
 )
   useEffect(() => {debouncedFetch(query)}, [query])
+  useEffect(() => {
+    if (selectedId === null) {
+      setSelectedProduct(null)
+      return
+    }
 
+    async function getProductDetails() {
+      try {
+        const response = await fetch(`http://localhost:3333/products/${selectedId}`)
+        if (!response.ok) {
+          console.error("Il prodotto selezionato non esiste")
+          setSelectedProduct(null)
+          return
+        }
+
+        const data = await response.json()
+        setSelectedProduct(data)
+      } catch (error) {
+        console.error(error)
+        setSelectedProduct(null)
+      }
+    }
+
+    getProductDetails()
+  }, [selectedId])
   return (
     <>
-      <input type='text' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Cerca prodotti'></input>
-      {query.trim() !== "" && suggestions.length > 0 && <div>{suggestions.map(product => <p key={product.id}>{product.name}</p> )}</div>}
+      <input type='text' value={query}  onChange={(e) => setQuery(e.target.value)} placeholder='Cerca prodotti'></input>
+      {query.trim() !== "" && suggestions.length > 0 && <div>{suggestions.map(product => <p key={product.id} onClick={() => {setSelectedId(product.id); setSuggestions([]); setQuery(product.name)}}>{product.name}</p> )}</div>}
+      {selectedProduct && <div><img src={selectedProduct.image} alt={selectedProduct.name}></img> <h4>{selectedProduct.name} {selectedProduct.price}</h4> <p>{selectedProduct.description}</p></div>}
     </>
   )
 }
